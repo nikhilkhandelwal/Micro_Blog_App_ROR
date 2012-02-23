4eeeba36e1513fd8cc7b38134b6d161c16048f17
Nikhil Khandelwal <nk.7189@gmail.com>
2012-02-24 03:19:16 +0530

    sign sign out finished'

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -84,7 +84,11 @@ describe "success" do
           post :create, :user => @attr
         end.should change(User, :count).by(1)
       end
-
+     it "should sign the user in" do
+        post :create, :user => @attr
+        controller.should be_signed_in
+      end
+	
       it "should redirect to the user show page" do
         post :create, :user => @attr
         response.should redirect_to(user_path(assigns(:user)))
