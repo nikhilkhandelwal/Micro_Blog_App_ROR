e190ff0cffb3b2dcac259db60be834cbea594823
Nikhil Khandelwal <nk.7189@gmail.com>
2012-02-24 20:05:42 +0530

    finish user edit, update and index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,21 @@
 class UsersController < ApplicationController
-  def new
+  before_filter :authenticate, :only => [:edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
+	def new
   @user = User.new	  
   @title = "Sign up"
   end
-  def show
-  @user = User.find(params[:id])
-  @title = @user.name
+  def index
+    @title = "All users"
+    @users = User.paginate(:page => params[:page])
   end
+
+  def show
+    @user = User.find(params[:id])
+    @title = @user.name
+  end
+	
+  
   def create
   @user = User.new(params[:user])
   if @user.save
@@ -19,4 +28,28 @@ class UsersController < ApplicationController
   render 'new'
   end
   end
+  def edit
+   
+    @title = "Edit user"
+  end
+def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated."
+      redirect_to @user
+    else
+      @title = "Edit user"
+      render 'edit'
+    end
+  end
+private 
+def authenticate
+      deny_access unless signed_in?
+    end
+
+def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+  
 end
