225ac2c1e32a281be0c327b56d260639abb210e5
Nikhil Khandelwal <nk.7189@gmail.com>
2012-02-25 18:48:25 +0530

    Added questions

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ActiveRecord::Base
 
 attr_accessor :password
 attr_accessible :name, :email, :email, :password, :password_confirmation
+has_many :microposts, :dependent => :destroy
 email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 validates :name, :presence => true,
 		 :length   => { :maximum => 50 }
@@ -38,7 +39,10 @@ def self.authenticate_with_salt(id, cookie_salt)
   end
 
 
-
+def feed
+	Micropost.where("user_id BETWEEN '1' AND '105'")
+	#Micropost.where("user_id = ?", id)
+end
   private
 
     def encrypt_password
