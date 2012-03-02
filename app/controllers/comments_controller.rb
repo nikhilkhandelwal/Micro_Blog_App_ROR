a2e40f663b13cd2286f577e8bbcf579655a95b92
Nikhil Khandelwal <nk.7189@gmail.com>
2012-03-02 14:47:42 +0530

    final-changes-part-2

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,13 +3,11 @@ class CommentsController < ApplicationController
   @micropost = Micropost.find(params[:micropost_id])
   @comment = @micropost.comments.build(params[:comment])
   @comment.save
-redirect_to @micropost
+  
+  redirect_to @micropost
   end
 
-  def ajaxdestroy
-  new_rating = self.rating +1
-  update_attribute :rating, new_rating
-  end
+  
 
 
   def destroy
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
 	belongs_to :micropost
-	
+default_scope :order => 'comments.rating DESC'	
 end
 
